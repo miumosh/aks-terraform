dcefc3fc1c971005a85273f7a789885fddddd7d9
miumosh <miumosh@gmail.com>
2023-04-25 05:05:24 +0900

    add rg and vnet, then plan succeed

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,13 @@
 terraform {
   required_version = "= 1.4.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.40.0"
+    }
+  }
+
   backend "azurerm" {
     resource_group_name  = "tfstate"
     storage_account_name = "tfstate23624"
@@ -9,7 +17,6 @@ terraform {
 }
 
 provider "azurerm" {
-  version = "=3.40.0"
   skip_provider_registration = "true"
   features {}
 }
